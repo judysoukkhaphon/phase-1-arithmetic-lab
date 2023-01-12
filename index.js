43d34b47a41d972b1f5d6b9552ac37d7e8c6d8f0
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-12 12:26:49 -0500

    wrote some math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+const num3 = 17;
+const num4 = 13;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 20);
